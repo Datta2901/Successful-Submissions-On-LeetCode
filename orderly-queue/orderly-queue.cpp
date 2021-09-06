7360994ad3f708bbee3366f4ddca10ad2d79c25f
manikanta2901 <54176498+manikanta2901@users.noreply.github.com>
2021-09-06 18:38:15 +0530

    Time: 4 ms (73.98%), Space: 10 MB (73.47%) - LeetHub

---
+++ orderly-queue/orderly-queue.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string GetString(string s){
+        string answer;
+        answer = s[s.size() - 1];
+        s.erase(s.end() - 1);
+        answer += s;
+        return answer;
+    }
+    string orderlyQueue(string s, int k) {
+        string mini = s,a = s;
+        if(k == 1){
+           for(int i = 0; i < s.size(); i++){
+               a = GetString(a);
+               //cout << a << endl;
+               mini = min(mini,a);
+           } 
+           return mini;
+        }
+        sort(s.begin(),s.end());
+        return s;
+    }
+};
