16a50cc837bdaa27e888bc5e90f0b8bdbdb484f7
manikanta2901 <54176498+manikanta2901@users.noreply.github.com>
2021-09-12 13:26:41 +0530

    Time: 4 ms (98.03%), Space: 7.4 MB (43.26%) - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()){
+        int n = s.size();
+        int m = t.size();
+        if(n != m){
             return false;
         }
         vector<int>mag(26,0);
-        for(int i = 0; i < s.size(); i++){
+        for(int i = 0; i < n; i++){
             mag[s[i] - 'a'] += 1;
         }
-        for(int i = 0; i < t.size(); i++){
+        for(int i = 0; i < m; i++){
             if(mag[t[i] - 'a'] > 0){
                 mag[t[i] - 'a'] -= 1;
             }else{
