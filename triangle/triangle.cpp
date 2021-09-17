56d5aa772960d12efde3bc52cd45d5ea5db2e6f7
manikanta2901 <54176498+manikanta2901@users.noreply.github.com>
2021-09-17 22:09:24 +0530

    Time: 4ms (89.04%), Space: 8.6 MB (45.50%)-LeetHub

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -4,7 +4,8 @@ public:
          int n=tri.size();
          for(int i=1;i<tri.size();i++){
              for(int j=0;j<tri[i].size();j++){
-                 if(j==0){ 
+                 if(j==0){
+                     
                      tri[i][j]=tri[i-1][j]+tri[i][j];
                 }else if(j==tri[i].size()-1){
                     tri[i][j]=tri[i-1][j-1]+tri[i][j];
@@ -14,7 +15,6 @@ public:
              }
               
          }
-         
         return *min_element(tri[n-1].begin(),tri[n-1].end());    
     }
 };
