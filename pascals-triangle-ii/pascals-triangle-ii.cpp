3b0498c669aaf092bc4eae11f0cd76076539b0b3
manikanta2901 <manikanta2901@gmail.com>
2021-09-22 17:12:53 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (52.62%) - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.cpp
+++ pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,3 +1,5 @@
+// This is not the ideal solution
+// Due to generating all the above rows of given kth row 
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
@@ -12,5 +14,28 @@ public:
         }
         return pascal[rowIndex];
         
+    }
+};
+
+
+// Efficient solution follows as
+// By using the formula 
+// given r and c are the row and column
+// r - 1 C
+//          c - 1
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        //By Using Combinations
+        vector<int>answer;
+        long long int nCr = 1;
+        answer.push_back(nCr);
+        rowIndex += 1;// 1 based
+        for(int i = 1; i < rowIndex; i++){
+            nCr = nCr * (rowIndex - i);
+            nCr = nCr / i;
+            answer.push_back(nCr);
+        }
+        return answer;
     }
 };
