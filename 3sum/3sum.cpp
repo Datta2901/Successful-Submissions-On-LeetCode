6534bca43764e4c1ed8cc2c0b1ea126e7436c5f1
manikanta2901 <54176498+manikanta2901@users.noreply.github.com>
2021-09-22 17:33:21 +0530

    Time: 92 ms (59.81%), Space: 23.6 MB (29.12%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -3,13 +3,9 @@ public:
     vector<vector<int>> threeSum(vector<int>& num) {
         vector<vector<int>> res; 
         sort(num.begin(), num.end()); 
-        
-        for (int i = 0; i < (int)(num.size())-2; i++) {
-            
-            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-                
+        for (int i = 0; i < (int)(num.size())-2; i++) {  
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {  
                 int lo = i+1, hi = (int)(num.size())-1, sum = 0 - num[i];
-                
                 while (lo < hi) {
                     if (num[lo] + num[hi] == sum) {
                         
@@ -18,7 +14,7 @@ public:
                         temp.push_back(num[lo]); 
                         temp.push_back(num[hi]); 
                         res.push_back(temp);
-                        
+  
                         while (lo < hi && num[lo] == num[lo+1]) lo++;
                         while (lo < hi && num[hi] == num[hi-1]) hi--;
                         
