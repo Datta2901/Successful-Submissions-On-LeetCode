efc9bac85b5549a01a7eb1a3b4af4dbbad1e1273
manikanta2901 <54176498+manikanta2901@users.noreply.github.com>
2021-09-25 18:48:12 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (48.27%) - LeetHub

---
+++ longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char,int> fre;
+        for(int i = 0; i < s.size(); i++){
+            fre[s[i]]++;
+        }
+        int count = 0;  
+        bool middle = true;
+        for(auto it : fre){
+            if(it.second % 2 == 0){
+                count += (it.second);
+            }else{
+                if(it.second >= 1 && middle){
+                    middle = false;
+                    count += 1;
+                }
+                count += ((it.second - 1));
+            }
+        }
+        return count;
+    }
+};
