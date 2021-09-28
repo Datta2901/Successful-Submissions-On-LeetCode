e2e942a6120b0837735e16bf8d9d6634a37d5ab8
manikanta2901 <54176498+manikanta2901@users.noreply.github.com>
2021-09-28 20:25:20 +0530

    Time: 8 ms (32.00%), Space: 8.4 MB (31.39%) - LeetHub

---
+++ subsets-ii/subsets-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        map<vector<int>,int> fre;
+        int size = 1 << nums.size();
+        vector<vector<int>> answer;
+        for(int i = 0; i < size; i++){
+            vector<int>a;
+            for(int j = 0; j < nums.size(); j++){
+                if(i & (1 << j)){
+                    a.push_back(nums[j]);
+                }
+            }
+            sort(a.begin(),a.end());        
+            if(fre[a] == 0){
+                answer.push_back(a);
+                fre[a] = 1;
+            }
+        }
+        return answer;       
+    }
+};
