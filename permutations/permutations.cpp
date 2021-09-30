7dab42b1d5fd435bcc71744654db8cb10dc3dfdd
manikanta2901 <54176498+manikanta2901@users.noreply.github.com>
2021-09-30 09:05:12 +0530

    Time: 4 ms (70.10%), Space: 7.8 MB (48.26%) - LeetHub

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
+    
     vector<vector<int> > answer;
-    void Backtracking(vector<int>&nums,int i,int size){
-        if(i == size){
-            answer.push_back(nums);
+    
+    void BackTrack(vector<int> &per ,vector<int> &nums, int start){
+        if(per.size() == nums.size()){
+            answer.push_back(per);
             return ;
         }
-        for(int j = i;  j <= size; j++){
-            swap(nums[i],nums[j]);
-            Backtracking(nums,i + 1,size);
-            swap(nums[i],nums[j]);
+        for(int i = 0; i < nums.size(); i++){
+            if(find(per.begin(),per.end(),nums[i]) != per.end()){
+                continue;
+            }
+            per.push_back(nums[i]);
+            BackTrack(per,nums,i + 1);
+            per.pop_back();
         }
+        
     }
-    vector<vector<int> > permute(vector<int>& nums) {
-        Backtracking(nums,0,nums.size() - 1);
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>per;
+        BackTrack(per,nums,0);
         return answer;
     }
 };
