a210d12cfb37eaae3d6993721ee9eaa8d806d7c0
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-10-03 14:40:13 +0530

    Time: 60 ms (70.06%), Space: 48.4 MB (49.19%) - LeetHub

---
+++ jump-game/jump-game.cpp
@@ -0,0 +1,21 @@
+class Solution{
+    public:
+        bool canJump(vector<int>& nums) {
+            int size = nums.size() - 1;
+            if(size == 0){
+                return true;
+            }
+            int answer = 0;
+            for(int i = 0; i < size; i++){
+                answer = max(answer, i + nums[i]);
+                if(answer == i){
+                    return false;
+                }else{
+                    if(answer >= size){
+                        return true;
+                    }
+                }
+            }
+            return true;
+        }
+};
