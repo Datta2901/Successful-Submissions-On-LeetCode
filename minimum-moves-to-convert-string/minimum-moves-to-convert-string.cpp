62fc89d1159249aa6e9f6290f072eede3b8a2c89
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-10-04 10:14:11 +0530

    Time: 8 ms (33.33%), Space: 6.4 MB (100.00%) - LeetHub

---
+++ minimum-moves-to-convert-string/minimum-moves-to-convert-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int count  = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == 'X'){
+                i = i + 2;
+                count++;
+            }
+        }
+        return count;
+    }
+};
