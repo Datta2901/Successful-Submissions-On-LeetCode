1871aaf92960a844311adfcb199e92e9ba8175ea
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-10-09 14:46:29 +0530

    Time: 172 ms (5.03%), Space: 44.8 MB (5.00%) - LeetHub

---
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+    string minRemoveToMakeValid(string s) {
+        string answer = "";
+        int open = 0;
+        int closed = 0;
+        map<int,int>index;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+               open++;
+            }else if(s[i] == ')'){
+                open--;
+                if(open < 0){
+                    index[i] = 1;
+                    open += 1;
+                }
+            }
+        }
+
+        // cout << open << endl;
+        for(int i = s.size() - 1; i >= 0; i--){
+            if(s[i] == '(' && open >= 1){
+                index[i] = 1;
+                open--;
+            }
+        }
+        // cout << open << endl;
+
+
+        for(int i = 0; i < s.size(); i++){
+            if(index[i] == 1){
+                s[i] = '@';
+            }
+        }
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != '@'){
+                answer += s[i];
+            }
+        }
+
+        return answer;
+    }
+};
