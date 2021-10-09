bb8f4087c49be25aa1e790eb20d04137a5a36504
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-10-09 14:51:11 +0530

    Time: 16 ms (97.58%), Space: 10.8 MB (78.87%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -43,3 +43,48 @@ public:
         return answer;
     }
 };
+
+
+
+// Space and time Optmization
+class Solution {
+public:
+
+    string minRemoveToMakeValid(string s) {
+        string answer = "";
+        int open = 0;
+        int closed = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+               open++;
+            }else if(s[i] == ')'){
+                open--;
+                if(open < 0){
+                    s[i] = '@';
+                    open += 1;
+                }
+            }
+        }
+
+        // cout << open << endl;
+        for(int i = s.size() - 1; i >= 0; i--){
+            if(s[i] == '(' && open >= 1){
+                s[i] = '@';
+                open--;
+            }
+        }
+        // cout << open << endl;
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != '@'){
+                answer += s[i];
+            }
+        }
+
+        return answer;
+    }
+};
+
+
+
+
