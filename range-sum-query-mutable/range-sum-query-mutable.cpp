fc933e269be203f4083bc055309e5e848e152809
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-10-28 20:16:04 +0530

    Using Fenwick Tree also added

--- range-sum-query-mutable/range-sum-query-mutable.cpp
+++ range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -70,3 +70,66 @@ public:
  * obj->update(index,val);
  * int param_2 = obj->sumRange(left,right);
  */
+
+
+// Using Binary Indexed Tree(Fenwick Tree)
+class NumArray {
+public:
+    vector<int>Fen,arr;
+    int size;
+
+    void build(vector<int>nums){
+        for(int i = 0; i < nums.size(); i++){
+            Buildupdate(i,nums[i]);
+        }
+    }
+    
+    NumArray(vector<int>& nums){
+        arr = nums;
+        Fen.resize(nums.size() + 1);
+        fill(Fen.begin(),Fen.end(),0);
+        size = arr.size();
+        build(nums);
+    }
+    
+    void Buildupdate(int index, int val) {
+        index += 1;
+        while(index <= size){
+            Fen[index] += val;
+            index = index + (index & (-index));
+        }
+    }
+
+    void update(int index, int val) {
+        val = val - arr[index];
+        arr[index] = val;
+        index += 1;
+        while(index <= size){
+            Fen[index] += val;
+            index = index + (index & (-index));
+        }
+    }
+    
+    int sumOneRange(int index){
+        index += 1;
+        int sum = 0;
+        while(index > 0){
+            sum += Fen[index];
+            index -= (index & -index);
+        }
+        return sum;
+    }
+
+    int sumRange(int left, int right){
+        int l = sumOneRange(left - 1);
+        int r = sumOneRange(right);
+        cout << l << " " << r << endl;
+        return  r - l ;
+    }
+    
+    void _printFen(){
+        for(int i = 1; i < Fen.size(); i++){
+            cout << Fen[i] << ' ';
+        }
+    }
+};
