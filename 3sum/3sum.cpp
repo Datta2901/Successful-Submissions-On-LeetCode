d0e0e35c4e5c0ae588d0fcf0b63d7edc7c986863
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-11-29 21:23:17 +0530

    Time: 88 ms (59.79%), Space: 22.1 MB (35.91%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -1,27 +1,44 @@
 class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& num) {
-        vector<vector<int>> res; 
-        sort(num.begin(), num.end()); 
-        for (int i = 0; i < (int)(num.size())-2; i++) {  
-            if (i == 0 || (i > 0 && num[i] != num[i-1])) {  
-                int lo = i+1, hi = (int)(num.size())-1, sum = 0 - num[i];
-                while (lo < hi) {
-                    if (num[lo] + num[hi] == sum) {  
-                        vector<int> temp; 
-                        temp.push_back(num[i]); 
-                        temp.push_back(num[lo]); 
-                        temp.push_back(num[hi]); 
-                        res.push_back(temp);
-                        while (lo < hi && num[lo] == num[lo+1]) lo++;
-                        while (lo < hi && num[hi] == num[hi-1]) hi--;  
-                        lo++; hi--;
-                    } 
-                    else if (num[lo] + num[hi] < sum) lo++;
-                    else hi--;
-               }
+private:
+    vector<vector<int>> answer;
+    void binarySearch(vector<int>&num,int index){
+        int n = num.size();
+        int low = index + 1,high = n - 1;
+        int sum = 0 - num[index];
+        while(low < high){
+            if(num[low] + num[high] == sum){
+                answer.push_back({num[low],num[high],num[index]});
+                while(low < high && num[low] == num[low + 1]){
+                    low++;
+                }
+                while(low < high && num[high] == num[high - 1]){
+                    high--;
+                }
+                low++,high--;
+            }else if(num[low] + num[high] < sum){
+                low++;
+            }else{
+                high--;
             }
         }
-        return res;
+    }
+public:
+    vector<vector<int>> threeSum(vector<int>& num) {
+        
+        //Using three pointers
+        int n = num.size();
+        if(n == 0){
+            return answer;
+        }
+        sort(num.begin(),num.end());
+        for(int i = 0; i < n - 2; i++){
+            // i - one pointer             
+            if(i == 0 || (num[i] != num[i - 1])){
+                // doing binary search on other side
+                // We have to avoid duplicates 
+                binarySearch(num,i);
+            }
+        }
+        return answer;
     }
 };
