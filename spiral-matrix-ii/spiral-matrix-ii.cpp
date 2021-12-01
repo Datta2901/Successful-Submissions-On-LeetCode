287f814b8c47950880eacf384a8d291243b5c694
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-12-01 15:19:28 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (63.73%) - LeetHub

--- spiral-matrix-ii/spiral-matrix-ii.cpp
+++ spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     vector<vector<int> > generateMatrix(int n) {
-        vector<vector<int> > answer(n, vector<int>(n));
-        int rowStart = 0, rowEnd = n - 1;
-        int colStart = 0, colEnd = n - 1;
+        vector<vector<int> >answer(n,vector<int>(n));
+        int rowStart = 0,rowEnd = n - 1;
+        int colStart = 0,colEnd = n - 1;
         int count = 1;
-    
-        while(rowStart <= rowEnd && colStart <= colEnd)
-        {
-            for(int i = colStart; i<= colEnd; i++)
+        while(rowStart <= rowEnd && colStart <= colEnd){
+            for(int i = colStart; i <= colEnd; i++){
                 answer[rowStart][i] = count++;
+            }
             rowStart++;
-        
-            for(int i = rowStart; i<= rowEnd; i++)
+            for(int i = rowStart; i <= rowEnd; i++){
                 answer[i][colEnd] = count++;
+            }
             colEnd--;
-        
-            for(int i = colEnd; i>= colStart; i--)
+            for(int i = colEnd; i >= colStart; i--){
                 answer[rowEnd][i] = count++;
+            }
             rowEnd--;
-        
-            for(int i = rowEnd; i>= rowStart; i--)
+            for(int i = rowEnd; i >= rowStart; i--){
                 answer[i][colStart] = count++;
+            }
             colStart++;
         }
-        return answer;
+        return answer;  
     }
 };
