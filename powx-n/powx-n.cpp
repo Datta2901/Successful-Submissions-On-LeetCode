c418f39636dae309e8a2bbbb4e91c59bac3296f7
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-12-07 17:19:30 +0530

    Time: 3 ms (15.00%), Space: 5.9 MB (80.12%) - LeetHub

--- powx-n/powx-n.cpp
+++ powx-n/powx-n.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double answer = pow(x,n);
+        // stl         
+        // double answer = pow(x,n);
+        double answer = 1;
+        long int times = n;
+        if(times < 0){
+            times = -times;
+        }
+        while(times > 0){
+            if(times % 2 == 1){
+                answer = answer * x;
+                times -= 1;
+            }else{
+                 x = x * x;
+                 times /= 2;
+            }
+        }
+        if(n < 0){
+            answer = 1.0 / answer;
+        }
         return answer;
     }
 };
