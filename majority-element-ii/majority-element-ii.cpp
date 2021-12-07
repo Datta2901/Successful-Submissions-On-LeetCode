ab40bf8428c9a767817cfe49ba3f95a5de8d57ac
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-12-07 19:34:24 +0530

    Time: 12 ms (75.37%), Space: 15.9 MB (48.94%) - LeetHub

--- majority-element-ii/majority-element-ii.cpp
+++ majority-element-ii/majority-element-ii.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& A) {
-        
-        int ele1 = INT_MAX,ele2 = INT_MIN,n = A.size();
-        int answer = n/3;
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int ele1 = INT_MAX,ele2 = INT_MAX;
         int count1 = 0,count2 = 0;
         for(int i = 0; i < n; i++){
-            if(A[i] == ele1){
-                count1++;
-            }else if(A[i] == ele2){
+            if(ele1 == nums[i]){
+               count1++; 
+            }else if(ele2 == nums[i]){
                 count2++;
             }else if(count1 == 0){
-                ele1 = A[i];
-                count1 += 1;
+                ele1 = nums[i];
+                count1++;
             }else if(count2 == 0){
-                ele2 = A[i];
-                count2 += 1;
+                ele2 = nums[i];
+                count2++;
             }else{
-                count1 -= 1;
-                count2 -= 1;
+                count1--;
+                count2--;
             }
         }
-        vector<int>aa;
-
+        vector<int>answer;
         if(count1 == 0 && count2 == 0){
-            return aa;
+            return {};
         }
-        count1 = 0,count2 = 0;
+        count1 = 0,count2  = 0;
         for(int i = 0; i < n; i++){
-            if(A[i] == ele1){
+            if(ele1 == nums[i]){
                 count1++;
-            }else if(A[i] == ele2){
+            }else if(ele2 == nums[i]){
                 count2++;
             }
         }
-        if(count1 > answer){
-            aa.push_back(ele1);
-        }if(count2 > answer){
-           aa.push_back(ele2);
+        if(count1 > n / 3){
+            answer.push_back(ele1);
+        }if(count2 > n / 3){
+            answer.push_back(ele2);
         }
-        return aa;
+        return answer;
     }
 };
