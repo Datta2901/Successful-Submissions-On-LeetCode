c32104a8ab6f4e25ae66c933e72c7885a995ecb6
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-12-13 15:06:02 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (20.94%) - LeetHub

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -1,18 +1,21 @@
 class Solution {
+private:
+    int n;
+    int getAnswer(vector<int>&nums,int index,vector<int>&dp){
+        if(index >= n){
+            return 0;
+        }
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        int Steal = nums[index] + getAnswer(nums,index + 2,dp);
+        int notSteal = getAnswer(nums,index + 1,dp);
+        return dp[index] = max(Steal,notSteal);
+    }
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>dp(nums.size(),0);
-        if(n == 0){
-            return 0;
-        }if(n == 1){
-            return nums[0];
-        }
-        dp[0] = nums[0];
-        dp[1] = max(nums[1],nums[0]);
-        for(int i = 2; i < n; i++){
-            dp[i] = max(dp[i - 1],dp[i - 2] + nums[i]);
-        }
-        return dp[n - 1];
+        n = nums.size();
+        vector<int>dp(n,-1);
+        return getAnswer(nums,0,dp);
     }
 };
