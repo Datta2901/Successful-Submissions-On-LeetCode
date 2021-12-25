9cb8668e0d8db2321588947210aede825568a56d
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-12-25 15:59:45 +0530

    Time: 8 ms (77.97%), Space: 14.6 MB (9.35%) - LeetHub

--- flood-fill/flood-fill.cpp
+++ flood-fill/flood-fill.cpp
@@ -1,51 +1,30 @@
 class Solution {
 public:
-    
     vector<vector<int> > floodFill(vector<vector<int> >& image, int sr, int sc, int newColor) {
         int m = image.size();
         int n = image[0].size();
-        vector<pair<int,int> >answer;
-        queue<pair<int,int> >possibility;
-        map<pair<int,int>,int > visited;
-        possibility.push({sr,sc});
-        while(!possibility.empty()){
-            pair<int,int>node =possibility.front();
-            if(visited[node] == 0){
-                // Up
-                if(node.first + 1 < m && node.second < n){
-                    if(image[node.first][node.second] == image[node.first + 1][node.second]){
-                        possibility.push({node.first + 1,node.second});
+        queue<pair<int,int> > track;
+        map<pair<int,int>,int>visited;
+        int dx[4] = {1,-1,0,0};
+        int dy[4] = {0,0,-1,1};
+        visited[{sr,sc}] = 1;
+        track.push({sr,sc});
+        while(!track.empty()){
+            int x = track.front().first;
+            int y = track.front().second;
+            track.pop();
+            for(int i = 0; i < 4; i++){
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+                if(visited[{newX,newY}] == 0){
+                    if(newX >= 0 && newX < m && newY >= 0 && newY < n && image[newX][newY] == image[x][y]){
+                        track.push({newX,newY});
+                        visited[{newX,newY}] = 1;
                     }
                 }
-                // Down
-                if(node.first - 1 >= 0 && node.first - 1 < m && node.second < n ){
-                    if(image[node.first][node.second] == image[node.first - 1][node.second]){
-                        possibility.push({node.first - 1,node.second});
-                    }
-                }
-
-                // Left
-                if(node.first  < m && node.second - 1 < n &&  node.second - 1 >= 0){
-                    if(image[node.first][node.second] == image[node.first][node.second - 1]){
-                        possibility.push({node.first,node.second - 1});
-                    }
-                }
-
-                // Right
-                if(node.first  < m && node.second + 1 < n){
-                    if(image[node.first][node.second] == image[node.first][node.second + 1]){
-                        possibility.push({node.first,node.second + 1});
-                    }
-                }
-                visited[node] = 1;
-                image[node.first][node.second] = newColor;
-                // answer.push_back(node);
             }
-            possibility.pop();   
+            image[x][y] = newColor;
         }
-        return image;
-    }
+        return image; 
+    }  
 };
-
-
-
