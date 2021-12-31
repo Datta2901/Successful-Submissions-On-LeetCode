889277ae7d311f35ac2b54b0dfa3e8a95eb15614
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2021-12-31 15:05:55 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (40.63%) - LeetHub

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -1,21 +1,44 @@
+// // Memoization
+// class Solution {
+// private:
+//     int n;
+//     int getAnswer(vector<int>&nums,int index,vector<int>&dp){
+//         if(index >= n){
+//             return 0;
+//         }
+//         if(dp[index] != -1){
+//             return dp[index];
+//         }
+//         int Steal = nums[index] + getAnswer(nums,index + 2,dp);
+//         int notSteal = getAnswer(nums,index + 1,dp);
+//         return dp[index] = max(Steal,notSteal);
+//     }
+// public:
+//     int rob(vector<int>& nums) {
+//         n = nums.size();
+//         vector<int>dp(n,-1);
+//         return getAnswer(nums,0,dp);
+//     }
+// };
+
+// Tabulation
+
 class Solution {
-private:
-    int n;
-    int getAnswer(vector<int>&nums,int index,vector<int>&dp){
-        if(index >= n){
-            return 0;
-        }
-        if(dp[index] != -1){
-            return dp[index];
-        }
-        int Steal = nums[index] + getAnswer(nums,index + 2,dp);
-        int notSteal = getAnswer(nums,index + 1,dp);
-        return dp[index] = max(Steal,notSteal);
-    }
 public:
     int rob(vector<int>& nums) {
-        n = nums.size();
+        int n = nums.size();
         vector<int>dp(n,-1);
-        return getAnswer(nums,0,dp);
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return nums[0];
+        }
+        dp[0] = nums[0];
+        dp[1] = max(nums[0],nums[1]);
+        for(int i = 2; i < n; i++){
+            dp[i] = max(nums[i] + dp[i - 2],dp[i - 1]);
+        }
+        return dp[n - 1];
     }
 };
