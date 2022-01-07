80c49ad0ae31244e3588e235e6826b019c9cde11
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-01-07 19:14:42 +0530

    Time: 11 ms (49.26%), Space: 11.1 MB (53.94%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int> > answer;
+    void GetAnswer(vector<int>&ds,vector<int>&nums,int ind,int sum,int target){
+        if(sum > target){
+            return;
+        }
+        if(ind >= nums.size()){
+            return;
+        }
+        if(sum == target){
+            answer.push_back(ds);
+            return;
+        }
+
+        // pick
+        if(target >= nums[ind]){
+            ds.push_back(nums[ind]);
+            sum += nums[ind];
+            GetAnswer(ds,nums,ind,sum,target);
+            sum -= nums[ind];
+            ds.pop_back();
+        }
+
+        // not pick
+        GetAnswer(ds,nums,ind + 1,sum,target);
+    }
+    
+    vector<vector<int> > combinationSum(vector<int>& candidates, int target) {
+        vector<int>ds;
+        GetAnswer(ds,candidates,0,0,target);
+        return answer;
+    }
+};
