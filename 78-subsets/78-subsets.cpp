29d3c6d5a68e8721b5632efd2c08a1ab31648485
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-01-07 17:45:04 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (61.44%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    vector<vector<int> >answer;
+    void printSubsets(vector<int>&nums){
+        for(auto &it : nums){
+            cout << it << " ";
+        }
+        cout << endl;
+    }
+
+    void getSubsets(int ind,vector<int>&sub,vector<int>&nums){
+        if(ind >= nums.size()){
+            // printSubsets(sub);
+            answer.push_back(sub);
+            return;
+        }
+        //pick this current element to our subset
+        sub.push_back(nums[ind]); 
+        getSubsets(ind + 1,sub,nums);
+        sub.pop_back();
+
+        // not pick the current element to our subet
+        getSubsets(ind + 1,sub,nums);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> sub;
+        getSubsets(0,sub,nums);
+        return answer;
+    }
+};
