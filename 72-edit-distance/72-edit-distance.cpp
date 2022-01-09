a06c5237ad5e56bfb8edfaea563240e4040a2485
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-01-09 13:13:16 +0530

    Time: 20 ms (35.32%), Space: 8.8 MB (60.45%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,26 +1,28 @@
 class Solution {
-    vector<vector<int> > dp;
-    int EditDistanceMemo(string &A,string &B,int i, int j){
-        if(i == 0){
-            return j;
+    int EditDistanceTab(string A,string B){
+        int m = A.size();
+        int n = B.size();
+        vector<vector<int> > dp(m + 1,vector<int>(n + 1,0));
+        for(int i = 0; i <= m; i++){
+            dp[i][0] = i;
         }
-        if(j == 0){
-            return i;
+        for(int j = 0; j <= n; j++){
+            dp[0][j] = j;
         }
-        if(dp[i - 1][j - 1] != -1){
-            return dp[i - 1][j - 1];
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(A[i - 1] == B[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = 1 + min({dp[i - 1][j],dp[i][j - 1],dp[i - 1][j - 1]});
+                }
+            }
         }
-        if(A[i - 1] == B[j - 1]){
-            return dp[i - 1][j - 1] = EditDistanceMemo(A,B,i - 1,j - 1);
-        }
-        int answer = 1 +  min({EditDistanceMemo(A,B,i - 1,j),EditDistanceMemo(A,B,i,j - 1),EditDistanceMemo(A,B,i - 1,j - 1)});
-        return dp[i - 1][j - 1] = answer;
+        return dp[m][n];
     }
   
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int> > dp1(word1.size(),vector<int>(word2.size(),-1));
-        dp = dp1;
-        return EditDistanceMemo(word1,word2,word1.size(),word2.size());
+        return EditDistanceTab(word1,word2);
     }
 };
