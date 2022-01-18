b96411ce314275357a123ba755ef13c42c595ecb
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-01-18 12:19:30 +0530

    Time: 339 ms, Memory: 67.2 MB - LeetHub

--- k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,14 +1,28 @@
 class Solution {
+private:
+    double getDistance(vector<int>&dis){
+        return sqrt(pow(dis[0],2) + pow(dis[1],2));
+    }
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
-        vector<vector<int>> answer;
-        vector<pair<double,vector<int>>> distances;
-        for(auto i:points){
-            double distance=sqrt(pow(i[0],2)+pow(i[1],2));
-            distances.push_back(make_pair(distance,i));
+        vector<vector<int> > answer;
+        priority_queue<pair<double,int>,vector<pair<double,int> >,greater<pair<double,int>> > pq; 
+        int m = points.size();
+        int n = points[0].size();
+        for(int i = 0; i < m; i++){
+            double dis = getDistance(points[i]);
+            pq.push({dis,i});
+            // cout << dis << " " << i << endl;
         }
-        sort(distances.begin(),distances.end());
-        for(int i=0;i<K;i++)answer.push_back(distances[i].second);
+        int count = 0;
+        while(!pq.empty()){
+            int s = pq.top().second;
+            if(count < K){
+                answer.push_back({points[s][0],points[s][1]});
+            }
+            pq.pop();
+            count++;
+        }
         return answer;
     }
 };
