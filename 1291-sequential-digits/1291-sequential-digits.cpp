54b7cf337d6282b3904978577a37fae9518b8ce7
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-01-23 19:14:41 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (11.79%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    string getSub(int start,int end,string s){
+        string answer = "";
+        for(int i = start; i < end; i++){
+            answer += (s[i]);
+        }
+        return answer;
+    }
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int>answer;
+        string digits = "123456789";
+        int dl = log10(low) + 1;
+        int dh = log10(high) + 1;
+        for(int i = dl; i <= dh; i++){
+            for(int j = 0; j < 9; j++){
+                // int num = stoi(digits.substr(j,j + i));
+                if(j + i <= 9){
+                    int num = stoi(getSub(j,j + i,digits));
+                    // cout << num << endl;
+                    if(low <= num && num <= high){
+                        answer.push_back(num);
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+};
