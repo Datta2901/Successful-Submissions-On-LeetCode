abe83a8936c19a1cd72ed09d17cae24565c0c05c
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-01-26 22:50:45 +0530

    Time: 12 ms, Memory: 14 MB - LeetHub

--- delete-and-earn/delete-and-earn.cpp
+++ delete-and-earn/delete-and-earn.cpp
@@ -1,56 +1,56 @@
+// // Using fre
 // class Solution {
 // private:
 //     map<int,int>fre;
 // public:
-//     int getAnswer(vector<int>&nums,int index,vector<int>&dp){
-//         if(index >= nums.size()){
-//             return 0;
-//         }
-//         if(dp[index] != -1){
-//             return dp[index];
-//         }
-//         int pick = nums[index] + getAnswer(nums,index + 2,dp);
-//         int notPick = getAnswer(nums,index + 1,dp);
-//         return dp[index] = max(pick,notPick);
-//     }
     
 //     int deleteAndEarn(vector<int>& nums) {
 //         int maxi = INT_MIN;
+//         int n = nums.size();
 //         for(auto it : nums){
 //             fre[it]++;
 //             maxi = max(maxi,it);
 //         }
-//         vector<int>dp(maxi + 1,-1);
+//         // cout << maxi << endl;
         
-//         return getAnswer(nums,0,dp);
+//         vector<int>dp(maxi + 1,0);
+        
+//         dp[1] = fre[1];
+//         if(n == 1){
+//             return fre[nums[0]] * nums[0];
+//         }
+//         dp[2] = max(dp[1],fre[2] * 2);
+//         for(int i = 3; i <= maxi; i++){
+//             dp[i] = max(dp[i - 1],dp[i - 2] + fre[i] * (i));
+//         }
+        
+//         return dp[maxi];
 //     }
 // };
 
+
+// Using fre
 class Solution {
 private:
-    map<int,int>fre;
+    int n = 10001;
 public:
     
     int deleteAndEarn(vector<int>& nums) {
-        int maxi = INT_MIN;
-        int n = nums.size();
-        for(auto it : nums){
-            fre[it]++;
-            maxi = max(maxi,it);
+        vector<int>sum(n,0),dp(n,0);
+        for(int i = 0; i < nums.size(); i++){
+            int num = nums[i];
+            sum[num] += num;
         }
-        // cout << maxi << endl;
-        
-        vector<int>dp(maxi + 1,0);
-        
-        dp[1] = fre[1];
-        if(n == 1){
-            return fre[nums[0]] * nums[0];
+        dp[0] = 0;
+        dp[1] = sum[1];
+        for(int i = 2; i < n; i++){
+            dp[i] = max(dp[i - 1], dp[i - 2] + sum[i]);
         }
-        dp[2] = max(dp[1],fre[2] * 2);
-        for(int i = 3; i <= maxi; i++){
-            dp[i] = max(dp[i - 1],dp[i - 2] + fre[i] * (i));
-        }
-        
-        return dp[maxi];
+        return dp[n - 1];
     }
 };
+
+
+
+
+
