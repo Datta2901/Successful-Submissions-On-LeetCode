47c19f57a031481bbac9334149cb9d18238a94f7
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-01-26 22:39:43 +0530

    Time: 18 ms, Memory: 10.2 MB - LeetHub

--- delete-and-earn/delete-and-earn.cpp
+++ delete-and-earn/delete-and-earn.cpp
@@ -1,23 +1,56 @@
-class Solution {
-public:
-    int deleteAndEarn(vector<int>& nums) {
-        int include = 0;
-        int exclude = 0;
-        int n = nums.size();
-        int maxi = *max_element(nums.begin(),nums.end());
-        vector<int> dp(maxi + 1,0);
+// class Solution {
+// private:
+//     map<int,int>fre;
+// public:
+//     int getAnswer(vector<int>&nums,int index,vector<int>&dp){
+//         if(index >= nums.size()){
+//             return 0;
+//         }
+//         if(dp[index] != -1){
+//             return dp[index];
+//         }
+//         int pick = nums[index] + getAnswer(nums,index + 2,dp);
+//         int notPick = getAnswer(nums,index + 1,dp);
+//         return dp[index] = max(pick,notPick);
+//     }
+    
+//     int deleteAndEarn(vector<int>& nums) {
+//         int maxi = INT_MIN;
+//         for(auto it : nums){
+//             fre[it]++;
+//             maxi = max(maxi,it);
+//         }
+//         vector<int>dp(maxi + 1,-1);
         
-        for(int i = 0; i < n; i++){
-            dp[nums[i]]++;
-        }
-        
-        for(int i = 0; i <= maxi; i++){
-            int tempInclude =  exclude + dp[i] * i;
-            int tempExclude = max(include,exclude);
-            include = tempInclude;
-            exclude = tempExclude ;
+//         return getAnswer(nums,0,dp);
+//     }
+// };
 
+class Solution {
+private:
+    map<int,int>fre;
+public:
+    
+    int deleteAndEarn(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int n = nums.size();
+        for(auto it : nums){
+            fre[it]++;
+            maxi = max(maxi,it);
         }
-        return max(include,exclude);
+        // cout << maxi << endl;
+        
+        vector<int>dp(maxi + 1,0);
+        
+        dp[1] = fre[1];
+        if(n == 1){
+            return fre[nums[0]] * nums[0];
+        }
+        dp[2] = max(dp[1],fre[2] * 2);
+        for(int i = 3; i <= maxi; i++){
+            dp[i] = max(dp[i - 1],dp[i - 2] + fre[i] * (i));
+        }
+        
+        return dp[maxi];
     }
 };
