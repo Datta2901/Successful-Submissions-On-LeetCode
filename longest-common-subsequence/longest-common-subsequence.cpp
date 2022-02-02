8f103979e073a90b0e38b625d684b29d9d27c8e9
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-02-02 16:39:26 +0530

    Time: 8 ms, Memory: 10.7 MB - LeetHub

--- longest-common-subsequence/longest-common-subsequence.cpp
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -27,3 +27,26 @@ public:
         return LCS(text1,text2);
     }
 };
+
+// class Solution{
+//     int m,n;
+//     int getAnswer(string a,string b,int i,int j,vector<vector<int> > &dp){
+//         if(i == m || j == n){
+//             return 0;
+//         }
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         }
+//         if(a[i] == b[j]){
+//             return dp[i][j] =  1 + getAnswer(a,b,i + 1,j + 1,dp);
+//         }
+//         return dp[i][j] = max(getAnswer(a,b,i + 1,j,dp),getAnswer(a,b,i,j + 1,dp));
+//     }
+//   public:
+//     int longestCommonSubsequence(string a,string b){
+//         m = a.size();
+//         n = b.size();
+//         vector<vector<int> > dp(m + 1,vector<int>(n + 1,-1));
+//         return getAnswer(a,b,0,0,dp);
+//     }
+// };
