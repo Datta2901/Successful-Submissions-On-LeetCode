037143a1c947597d343a0f045bfa9208066d6b4b
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-02-07 10:04:18 +0530

    Time: 6 ms (28.68%), Space: 6.6 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -18,21 +18,38 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     char findTheDifference(string s, string t) {
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         int n = s.size();
+//         int i = 0;
+//         for(i = 0; i < n; i++){
+//             if(s[i] != t[i]){
+//                 return t[i];
+//             }
+//         }    
+//         if(i == n){
+//            return t[n]; 
+//         }
+//         return 'a';
+//     }
+// };
+
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        char r=0;
         int n = s.size();
-        int i = 0;
-        for(i = 0; i < n; i++){
-            if(s[i] != t[i]){
-                return t[i];
-            }
-        }    
-        if(i == n){
-           return t[n]; 
+        for(int i = 0; i < n; i++){
+            r = r ^ s[i]; 
+            // cout << r << " ";
         }
-        return 'a';
+        n = t.size();
+        for(int j = 0; j < n; j++){
+            r = r ^ t[j];
+        }
+        return r;
     }
 };
