9d159b5f696e8af56d260fe5e2305705e1ee3deb
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-02-07 09:58:04 +0530

    Time: 7 ms (24.08%), Space: 6.7 MB (67.00%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,18 +1,37 @@
+// class Solution {
+// public:
+//     char findTheDifference(string s, string t) {
+//         map<char,int>fre;
+//         for(auto it : s){
+//             fre[it]++;
+//         }
+//         for(auto it : t){
+//             fre[it]--;
+//         }
+//         for(auto it : fre){
+//             // cout << it.first << " " << it.second << endl;
+//             if(it.second < 0){
+//                 return it.first;
+//             }
+//         }
+//         return 'a';
+//     }
+// };
+
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        map<char,int>fre;
-        for(auto it : s){
-            fre[it]++;
-        }
-        for(auto it : t){
-            fre[it]--;
-        }
-        for(auto it : fre){
-            // cout << it.first << " " << it.second << endl;
-            if(it.second < 0){
-                return it.first;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int n = s.size();
+        int i = 0;
+        for(i = 0; i < n; i++){
+            if(s[i] != t[i]){
+                return t[i];
             }
+        }    
+        if(i == n){
+           return t[n]; 
         }
         return 'a';
     }
