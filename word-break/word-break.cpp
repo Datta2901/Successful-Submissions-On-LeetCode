6f6495378df3e89a59ee4a6f90b88307f3dbd28b
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-02-13 18:13:31 +0530

    Time: 69 ms, Memory: 18.9 MB - LeetHub

--- word-break/word-break.cpp
+++ word-break/word-break.cpp
@@ -1,15 +1,29 @@
 class Solution {
+    unordered_map<string,int> fre;
+    bool getAnswer(int i,string s,vector<string>&wordDict,vector<int>&dp){
+        if(i == s.size()){
+            return true;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        for(int j = i + 1;j <= s.size(); j++){
+            string sub = s.substr(i,j - i);
+            if(fre[sub] == 1){
+                if(getAnswer(j,s,wordDict,dp)){
+                    return dp[i] = true;
+                }
+            }
+        }
+        return dp[i] = false;
+    }
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<bool> dp(s.size()+1, false);
-        dp[0] = true;
-        
-        for (int i = 1; i <= s.size(); i++)
-            for (int j = 0; j < i; j++)
-                if ((dp[j]) && (find(wordDict.begin(), wordDict.end(), s.substr(j, i-j)) != wordDict.end())) {
-                    dp[i] = true;
-                    break;
-                }
-        return dp.back();
+        for(auto i : wordDict){
+            fre[i]++;
+        }
+        int n = s.size();
+        vector<int>dp(n,-1);
+       return getAnswer(0,s,wordDict,dp);
     }
 };
