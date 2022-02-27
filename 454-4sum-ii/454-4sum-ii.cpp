c4db59d395047d9b40af8f1c16a70fa7c5c86634
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-02-27 14:43:26 +0530

    Time: 498 ms (23.16%), Space: 24.9 MB (19.46%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,19 +1,61 @@
 
+// class Solution {
+// public:
+    
+//     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+//         int count=0;
+//         unordered_map<int,int> mp;    
+//         for (auto &it1: nums1)
+//             for (auto &it2:nums2)
+//                 mp[it1+it2]++; 
+     
+//         for(auto &it3: nums3)
+//             for(auto &it4:nums4)
+//                if(mp.count(0-it3-it4)) count+=mp[0-it3-it4];
+        
+//         return count;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int answer = 0;
+        map<int,int>mp;
+        for(auto it : nums1){
+            for(auto i : nums2){
+                mp[it + i]++;
+            }
+        }
         
-        int count=0;
-        unordered_map<int,int> mp;    
-        for (auto &it1: nums1)
-            for (auto &it2:nums2)
-                mp[it1+it2]++; 
-     
-        for(auto &it3: nums3)
-            for(auto &it4:nums4)
-               if(mp.count(0-it3-it4)) count+=mp[0-it3-it4];
-        
-        return count;
+        for(auto it : nums3){
+            for(auto i : nums4){
+                answer += mp[0 - it - i];
+            }
+        }
+        return answer;
     }
 };
