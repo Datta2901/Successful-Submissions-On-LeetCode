fb4ab937ef127099daf9bb4212cac7042aa8afaf
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-02-27 15:10:11 +0530

    Time: 14 ms (18.04%), Space: 11 MB (38.71%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,22 +1,36 @@
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int>answer;
+//         int count = 1;
+//         answer.push_back(nums[0]);
+//         for(int i = 1; i < n; i++){
+//             if(nums[i] == nums[i - 1]){
+//                 count++;
+//                 if(count < 3){
+//                     answer.push_back(nums[i]);
+//                 }
+//             }else{
+//                 count = 1;
+//                 answer.push_back(nums[i]);
+//             }
+//         }
+//         nums = answer;
+//         return answer.size();
+//     }
+// };
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>answer;
-        int count = 1;
-        answer.push_back(nums[0]);
-        for(int i = 1; i < n; i++){
-            if(nums[i] == nums[i - 1]){
-                count++;
-                if(count < 3){
-                    answer.push_back(nums[i]);
-                }
-            }else{
-                count = 1;
-                answer.push_back(nums[i]);
+        int i = 0;
+        for(auto it : nums){
+            if(i < 2 || it > nums[i - 2]){
+                nums[i++] = it;
             }
+                
         }
-        nums = answer;
-        return answer.size();
+        return i;
     }
 };
