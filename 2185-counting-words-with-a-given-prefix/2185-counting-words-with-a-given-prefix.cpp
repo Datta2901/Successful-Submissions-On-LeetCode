8c52e10575fbfd95752faf9b53f50575081fd0eb
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-02-28 13:43:01 +0530

    Time: 4 ms (100.00%), Space: 10 MB (77.78%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,11 +1,26 @@
+// class Solution {
+// public:
+//     int prefixCount(vector<string>& words, string pref) {
+//         int n = words.size();
+//         int count = 0;
+//         for(int i = 0; i < n; i++){
+//             auto it = words[i].find(pref);
+//             if(it == 0){
+//                 count++;
+//             }
+//         }
+//        return count; 
+//     }
+// };
+
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
         int n = words.size();
         int count = 0;
         for(int i = 0; i < n; i++){
-            auto it = words[i].find(pref);
-            if(it == 0){
+            string st = words[i].substr(0,pref.size());
+            if(st == pref){
                 count++;
             }
         }
