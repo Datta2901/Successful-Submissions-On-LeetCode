4420a3e469186d0754fada8612ffe061843ec6a1
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-03-01 11:52:32 +0530

    Time: 8 ms (52.50%), Space: 8.5 MB (21.58%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,41 +1,20 @@
-// TimeComplexity O(nlogn)
-// class Solution {
-//     int getOneCount(int n){
-//         int count = 0;
-//         while(n){
-//             if(n & 1){
-//                 count += 1;
-//             }
-//             n = n >> 1;
-//         }
-//         return count;
-//     }
-// public:
-//     vector<int> countBits(int n) {
-//         vector<int>answer;
-//         for(int i = 0; i <= n; i++){
-//             // answer.push_back(__builtin_popcount(i));
-//             answer.push_back(getOneCount(i));
-//         }
-//         return answer;
-//     }
-// };
-
-// TimeComplexity O(n)
 class Solution {
+    int getOneCount(int n){
+        int count = 0;
+        while(n){
+            if(n & 1){
+                count += 1;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
 public:
     vector<int> countBits(int n) {
-        vector<int>answer(n + 1,0);
-        if(n == 0){
-            return answer;
-        }
-        answer[0] = 0;
-        for(int i = 1; i <= n; i++){
-            if(i % 2 == 0){
-                answer[i] = answer[i / 2];
-            }else{
-                answer[i] = 1 + answer[i - 1];
-            }
+        vector<int>answer;
+        for(int i = 0; i <= n; i++){
+            // answer.push_back(__builtin_popcount(i));
+            answer.push_back(getOneCount(i));
         }
         return answer;
     }
