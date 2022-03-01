cacc35fa71004b7f0fe0e4695fdbc2511f098221
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-03-01 12:58:53 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (69.07%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int m = s1.size();
+        int n = s2.size();
+        int count = 0;
+        char p1 = ' ',p2 = ' ';
+        for(int i = 0; i < m; i++){
+            if(s1[i] != s2[i]){
+                if(count == 0){
+                    p1 = s1[i];
+                    p2 = s2[i];
+                }else{
+                    if(p1 != s2[i] || p2 != s1[i]){
+                        return false;
+                    }
+                }
+                count++;
+            }
+        }
+        return (count == 2 || count == 0) ? true : false;
+    }
+};
