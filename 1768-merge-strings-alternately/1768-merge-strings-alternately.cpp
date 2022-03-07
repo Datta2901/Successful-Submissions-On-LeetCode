b6635bc38210ebcff2a890b515cc8c45e9d4d01f
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-03-07 12:45:35 +0530

    Time: 5 ms (22.75%), Space: 6.3 MB (57.25%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,27 +1,48 @@
+// class Solution {
+// public:
+//     string mergeAlternately(string word1, string word2) {
+//         int m = word1.size();
+//         int n = word2.size();
+//         bool turn = false;
+//         string answer = "";
+//         int i = 0,j = 0;
+//         while(1){
+//             if(!turn && i < m){
+//                 answer += word1[i];
+//                 i++;
+//                 turn = true;
+//             }else if(turn &&  j < n){
+//                 answer += word2[j];
+//                 j++;
+//                 turn = false;
+//             }
+//             if(i == m && j == n){
+//                 break;
+//             }if(i == m){
+//                 turn = true;
+//             }else if(j == n){
+//                 turn = false;
+//             }
+//         }
+//         return answer;
+//     }
+// };
+
+
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         int m = word1.size();
         int n = word2.size();
-        bool turn = false;
         string answer = "";
         int i = 0,j = 0;
-        while(1){
-            if(!turn && i < m){
+        while(i < m || j < n){
+            if(i < m){
                 answer += word1[i];
                 i++;
-                turn = true;
-            }else if(turn &&  j < n){
+            }if(j < n){
                 answer += word2[j];
                 j++;
-                turn = false;
-            }
-            if(i == m && j == n){
-                break;
-            }if(i == m){
-                turn = true;
-            }else if(j == n){
-                turn = false;
             }
         }
         return answer;
