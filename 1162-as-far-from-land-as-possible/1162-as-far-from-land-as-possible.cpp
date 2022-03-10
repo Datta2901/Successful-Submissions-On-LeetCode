7608bb055c70ef29d964ae34aa26f8ad511ec755
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-03-10 21:25:41 +0530

    Time: 77 ms (68.63%), Space: 19.7 MB (73.02%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,3 +1,42 @@
+// class Solution {
+//     int m,n;
+// public:
+//     int maxDistance(vector<vector<int>>& grid) {
+//         m = grid.size();
+//         n = grid[0].size();
+//         queue<pair<int,int> > store;
+//         for(int i = 0; i < m; i++){
+//             for(int j = 0; j < n; j++){
+//                 if(grid[i][j] == 1){
+//                     distance[i][j] = 0;
+//                     store.push({i,j});
+//                 }
+//             }
+//         }
+//         int answer = -1;
+//         int dx[4] = {0,0,1,-1};
+//         int dy[4] = {1,-1,0,0};
+//         while(!store.empty()){
+//             int X = store.front().first;
+//             int Y = store.front().second;
+//             store.pop();
+//             for(int i = 0; i < 4; i++){
+//                 int newX = X + dx[i];
+//                 int newY = Y + dy[i];
+//                 if(newX < 0 || newX >= m || newY < 0 || newY >= n || grid[newX][newY] != 0){
+//                     continue;
+//                 }
+//                 distance[newX][newY] = distance[X][Y] + 1;
+//                 answer = max(answer,distance[newX][newY]);
+//                 store.push({newX,newY});
+//             }
+            
+//         }
+//         return answer;
+        
+//     }
+// };
+
 class Solution {
     int m,n;
 public:
@@ -5,11 +44,9 @@ public:
         m = grid.size();
         n = grid[0].size();
         queue<pair<int,int> > store;
-        vector<vector<int> > distance(m,vector<int>(n,INT_MAX));
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 1){
-                    distance[i][j] = 0;
                     store.push({i,j});
                 }
             }
@@ -17,23 +54,30 @@ public:
         int answer = -1;
         int dx[4] = {0,0,1,-1};
         int dy[4] = {1,-1,0,0};
+        int count = -1;
         while(!store.empty()){
-            int X = store.front().first;
-            int Y = store.front().second;
-            store.pop();
-            for(int i = 0; i < 4; i++){
-                int newX = X + dx[i];
-                int newY = Y + dy[i];
-                if(newX < 0 || newX >= m || newY < 0 || newY >= n || distance[newX][newY] != INT_MAX){
-                    continue;
+            count++;
+            int size = store.size();
+            while(size--){
+                int X = store.front().first;
+                int Y = store.front().second;
+                store.pop();
+                for(int i = 0; i < 4; i++){
+                    int newX = X + dx[i];
+                    int newY = Y + dy[i];
+                    if(newX < 0 || newX >= m || newY < 0 || newY >= n || grid[newX][newY] != 0){
+                        continue;
+                    }
+                    grid[newX][newY] = 2;
+                    store.push({newX,newY});
                 }
-                distance[newX][newY] = distance[X][Y] + 1;
-                answer = max(answer,distance[newX][newY]);
-                store.push({newX,newY});
             }
             
         }
-        return answer;
+        if(count == 0){
+            return -1;
+        }
+        return count;
         
     }
 };
