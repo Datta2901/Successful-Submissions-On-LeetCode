df314254beff3a783a04e3117759b92f4479ae79
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-03-14 21:19:56 +0530

    Time: 8 ms (62.74%), Space: 9.5 MB (45.10%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   string simplifyPath(string path) {
+        string res, tmp;
+        if(path.empty()) return res;
+        vector<string> stk;
+        stringstream ss(path);
+        while(getline(ss,tmp,'/')) {
+            if (tmp == "" or tmp == ".") continue;
+            if(tmp != "..") stk.push_back(tmp);
+            else if(!stk.empty()) stk.pop_back();
+        }
+        for(auto str : stk) res += "/"+str;
+        return res.empty() ? "/" : res;
+    }
+};
