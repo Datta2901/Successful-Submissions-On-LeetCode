5c1203fec1e6258f8306dffe8e48e35ec5d7bc73
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-03-15 18:42:04 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (43.29%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,34 +1,57 @@
+// class Solution {
+// public:
+//     vector<int> partitionLabels(string s) {
+//         unordered_map<char,int>fre;
+//         vector<int>answer;
+//         for(int i = 0; i < s.size(); i++){
+//             fre[s[i]]++;
+//         }
+//         string ans = "";
+//         for(int i = 0; i < s.size(); i++){
+//             bool add = true;
+//             if(fre[s[i]] - 1 == 0){
+//                 ans += s[i];
+//                 fre[s[i]]--;
+//                 for(int i = 0; i < ans.size(); i++){
+//                     if(fre[ans[i]] != 0){
+//                         add = false;
+//                         break;
+//                     }
+//                 }
+//                 if(add){
+//                     answer.push_back(ans.size());
+//                     ans = "";
+//                 }
+//             }else{
+//                 ans += s[i];
+//                 fre[s[i]]--;
+//             }
+//         }
+//         if(ans.size() > 0){
+//             answer.push_back(ans.size());
+//         }
+//         return answer;
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char,int>fre;
-        vector<int>answer;
+        int n = s.size();
+        int start = 0,end = 0;
+        vector<int>fre(26, 0),answer;
         for(int i = 0; i < s.size(); i++){
-            fre[s[i]]++;
+            fre[s[i] - 'a'] = i;
         }
-        string ans = "";
+        int mini = 0;
         for(int i = 0; i < s.size(); i++){
-            bool add = true;
-            if(fre[s[i]] - 1 == 0){
-                ans += s[i];
-                fre[s[i]]--;
-                for(int i = 0; i < ans.size(); i++){
-                    if(fre[ans[i]] != 0){
-                        add = false;
-                        break;
-                    }
-                }
-                if(add){
-                    answer.push_back(ans.size());
-                    ans = "";
-                }
-            }else{
-                ans += s[i];
-                fre[s[i]]--;
+            end = max(end,fre[s[i] - 'a']);
+            if(i == end){
+                answer.push_back(end - start + 1);
+                start = end + 1;
             }
-        }
-        if(ans.size() > 0){
-            answer.push_back(ans.size());
         }
         return answer;
     }
