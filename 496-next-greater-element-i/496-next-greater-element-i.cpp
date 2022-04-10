b7c992a10073bf581a986feb13c05d2fdbd7e02d
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-04-10 12:19:21 +0530

    Time: 11 ms (43.80%), Space: 8.8 MB (66.72%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,25 +1,92 @@
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+//         stack<int>st;
+//         unordered_map<int,int>mp; 
+//         for(int i = nums2.size() - 1; i >= 0; i--){
+//             while(!st.empty() && st.top()<=nums2[i]){
+//                 st.pop();
+//             }
+//             if(st.empty()){
+//                 mp[nums2[i]]=-1;
+                
+//             }else{
+//                 mp[nums2[i]]=st.top();
+//             }
+//             st.push(nums2[i]);
+//         }
+//         vector<int>out;
+//         for(int i=0;i<nums1.size();i++){
+//             out.push_back(mp[nums1[i]]);
+//         }
+//         return out;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Do the next Greater element on num2 and store answers in a map
+// Now iterate through the second array and push the answers in a vector
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        
-        stack<int>st;
-        unordered_map<int,int>mp; 
-        for(int i = nums2.size() - 1; i >= 0; i--){
-            while(!st.empty() && st.top()<=nums2[i]){
-                st.pop();
+        vector<int>answer;
+        unordered_map<int,int>mp;
+        stack<int>store;
+        int n = nums2.size();
+        for(int i = n - 1; i >= 0; i--){
+            while(!store.empty() && store.top() <= nums2[i]){
+                store.pop();
             }
-            if(st.empty()){
-                mp[nums2[i]]=-1;
-                
+            if(store.empty()){
+                mp[nums2[i]] = -1;
             }else{
-                mp[nums2[i]]=st.top();
+                mp[nums2[i]] = store.top();
             }
-            st.push(nums2[i]);
+            store.push(nums2[i]);
         }
-        vector<int>out;
-        for(int i=0;i<nums1.size();i++){
-            out.push_back(mp[nums1[i]]);
+        int m = nums1.size();
+        for(int i = 0; i < m; i++){
+            answer.push_back(mp[nums1[i]]);
         }
-        return out;
+        return answer;
     }
 };
