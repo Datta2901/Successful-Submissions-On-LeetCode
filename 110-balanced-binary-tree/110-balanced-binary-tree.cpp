0b8033d60c0356bc995b955c81aae491a80b2107
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-04-10 14:48:26 +0530

    Time: 28 ms (17.89%), Space: 21 MB (25.12%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -9,27 +9,50 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+//     bool ans;
+//     int height(TreeNode* root){
+//         if(root == NULL){
+//             return 0;
+//         }
+//         if(ans == false){
+//             return 0;
+//         }
+//         int hl = height(root->left);
+//         int hr = height(root->right);
+//         if(abs(hl - hr) > 1){
+//             ans = false;
+//         }
+//         return 1 + max(height(root->left),height(root->right));
+//     }
+    
+// public:
+//     bool isBalanced(TreeNode* root) {
+//         ans = true;
+//         int l = height(root);
+//         return ans;
+//     }
+// };
+
 class Solution {
-    bool ans;
+    bool answer;
     int height(TreeNode* root){
-        if(root == NULL){
+        if(!root){
             return 0;
         }
-        if(ans == false){
-            return 0;
+        int l = height(root->left);
+        int r = height(root->right);
+        if(abs(l - r) > 1){
+            answer = false;
+            return false;
         }
-        int hl = height(root->left);
-        int hr = height(root->right);
-        if(abs(hl - hr) > 1){
-            ans = false;
-        }
-        return 1 + max(height(root->left),height(root->right));
+        return 1 + max(l,r);
     }
     
 public:
     bool isBalanced(TreeNode* root) {
-        ans = true;
-        int l = height(root);
-        return ans;
+        answer = true;
+        height(root);
+        return answer;
     }
 };
