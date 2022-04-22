5314dcf2de9a77e1adf2c3a29c6c6962270038f8
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-04-22 22:27:59 +0530

    Time: 5 ms (7.01%), Space: 5.8 MB (68.78%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cpp
@@ -7,19 +7,38 @@
  * int guess(int num);
  */
 
+// class Solution {
+// public:
+//     int guessNumber(int n) {
+//         int start = 1 , end = n;
+//         while(start <= end){
+//             int mid = start + (end - start) / 2;
+//             int ans = guess(mid);
+//             if(ans == 0)
+//                 return mid;
+//             else if( ans == 1)
+//                 start = mid + 1;
+//             else 
+//                 end = mid ;
+//         }
+//         return 0;
+//     }
+// };
+
 class Solution {
 public:
     int guessNumber(int n) {
-        int start = 1 , end = n;
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            int ans = guess(mid);
-            if(ans == 0)
+        int low = 1, high = n;
+        while(low <= high){
+            int mid = low + ((high - low) / 2);
+            int answer = guess(mid);
+            if(answer == -1){
+                high = mid - 1;
+            } else if(answer == 0){
                 return mid;
-            else if( ans == 1)
-                start = mid + 1;
-            else 
-                end = mid ;
+            }else{
+                low = mid + 1;
+            }
         }
         return 0;
     }
