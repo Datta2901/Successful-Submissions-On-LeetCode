c2576d053ad03a5cf0c409cb90acb7d21ec1cba3
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-04-29 16:41:20 +0530

    Time: 8 ms (44.43%), Space: 5.7 MB (95.69%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,7 +1,28 @@
+// class Solution {
+// public:
+//     int arrangeCoins(int n) {
+//         int ans = int(sqrt(2 * (long int)n + 0.25) - 0.5);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int ans = int(sqrt(2 * (long int)n + 0.25) - 0.5);
-        return ans;
+        int low = 0,high = n;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            long long sum = (long long)(mid) * (mid + 1) / 2;
+         
+            if(sum == n){
+                return mid;
+            }
+            if(n > sum){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return high;
     }
 };
