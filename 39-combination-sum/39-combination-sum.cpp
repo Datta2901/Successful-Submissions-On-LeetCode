93db978606bc1901e2aa604209c6a230075ea5cb
Datta2901 <54176498+Datta2901@users.noreply.github.com>
2022-06-05 18:07:51 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (49.27%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,34 +1,66 @@
+// class Solution {
+// public:
+//     vector<vector<int> > answer;
+//     void GetAnswer(vector<int>&ds,vector<int>&nums,int ind,int sum,int target){
+//         if(sum > target){
+//             return;
+//         }
+//         if(ind >= nums.size()){
+//             return;
+//         }
+//         if(sum == target){
+//             answer.push_back(ds);
+//             return;
+//         }
+
+//         // pick
+//         if(target >= nums[ind]){
+//             ds.push_back(nums[ind]);
+//             sum += nums[ind];
+//             GetAnswer(ds,nums,ind,sum,target);
+//             sum -= nums[ind];
+//             ds.pop_back();
+//         }
+
+//         // not pick
+//         GetAnswer(ds,nums,ind + 1,sum,target);
+//     }
+    
+//     vector<vector<int> > combinationSum(vector<int>& candidates, int target) {
+//         vector<int>ds;
+//         GetAnswer(ds,candidates,0,0,target);
+//         return answer;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<vector<int> > answer;
-    void GetAnswer(vector<int>&ds,vector<int>&nums,int ind,int sum,int target){
-        if(sum > target){
-            return;
-        }
-        if(ind >= nums.size()){
-            return;
-        }
-        if(sum == target){
-            answer.push_back(ds);
+    
+    void AllsubsequencesHavingSumk(vector<int>&nums,vector<int>&ds,int i,int sum,int k){
+        if(i == nums.size()){
+            if(k == 0){
+                answer.push_back(ds);
+            }
             return;
         }
 
-        // pick
-        if(target >= nums[ind]){
-            ds.push_back(nums[ind]);
-            sum += nums[ind];
-            GetAnswer(ds,nums,ind,sum,target);
-            sum -= nums[ind];
+        // Pick
+        if(nums[i] <= k){
+            ds.push_back(nums[i]);
+            AllsubsequencesHavingSumk(nums,ds,i,sum,k - nums[i]);
             ds.pop_back();
         }
 
-        // not pick
-        GetAnswer(ds,nums,ind + 1,sum,target);
+        // notpick
+        AllsubsequencesHavingSumk(nums,ds,i + 1,sum,k);
     }
-    
-    vector<vector<int> > combinationSum(vector<int>& candidates, int target) {
+     
+    vector<vector<int> > combinationSum(vector<int>& nums,int k){
         vector<int>ds;
-        GetAnswer(ds,candidates,0,0,target);
+        AllsubsequencesHavingSumk(nums,ds,0,0,k);
         return answer;
     }
 };
+
